62f02c397057bd9c773dc4aa67f45235edce88e8
dpinezich <david.pinezich@gmail.com>
2023-10-31 08:05:47 +0100

    updated exercise

--- vue/04_vue_todo/exercises/src/App.vue
+++ vue/04_vue_todo/exercises/src/App.vue
@@ -84,12 +84,42 @@ export default {
           @keyup.enter="addTodo"
       />
     </header>
-    <!-- your code here -->
-
-  </section>
-  <!--
-  a bit later
-      <footer class="footer" v-show="todos.length">
+    <section class="main" v-show="todos.length">
+      <input
+          id="toggle-all"
+          class="toggle-all"
+          type="checkbox"
+          v-model="allDone"
+      />
+      <ul class="todo-list">
+        <li
+            class="todo"
+            v-for="todo in filteredTodos"
+            :key="todo.id"
+            :class="{ completed: todo.completed, editing: todo === editedTodo }"
+        >
+          <div class="view">
+            <input
+                class="toggle"
+                type="checkbox"
+                @click="updateTodo(todo)"
+                :checked="todo.completed"
+            />
+            <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
+            <button class="destroy" @click="removeTodo(todo)"></button>
+          </div>
+          <input
+              class="edit"
+              type="text"
+              v-model="todo.title"
+              @blur="doneEdit(todo)"
+              @keyup.enter="doneEdit(todo)"
+              @keyup.esc="cancelEdit(todo)"
+          />
+        </li>
+      </ul>
+    </section>
+    <footer class="footer" v-show="todos.length">
       <span class="todo-count">
         <strong v-text="remaining"></strong>
         {{ pluralize('item', remaining) }} left
@@ -139,7 +169,6 @@ export default {
       <a href="https://todomvc.com/examples/vue/">TodoMVC - Vue</a>
     </p>
   </footer>
-  -->
 </template>
 
 <style>
